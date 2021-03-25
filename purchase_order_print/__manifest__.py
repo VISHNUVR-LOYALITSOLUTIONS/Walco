5bb723e0130f5e3d95877ca2bd0f6abd616e3dae
Hasna <73275743+hasna-has@users.noreply.github.com>
2021-03-25 10:45:26 +0530

    Add files via upload

--- purchase_order_print/__manifest__.py
+++ purchase_order_print/__manifest__.py
@@ -9,8 +9,8 @@
         PO Print
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Loyal ItSolutions PVT LTD",
+    'website': "http://www.loyalitsolutions.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/lay_out.xml',
         'views/templates.xml',
         'views/views.xml',
 
