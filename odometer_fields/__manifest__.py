a660b3ab530eb47734c3fa9fc7ec2c24dc3408f7
Hasna <73275743+hasna-has@users.noreply.github.com>
2021-05-22 16:05:37 +0530

    Add files via upload

---
+++ odometer_fields/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odometer_fields",
+
+    'summary': """
+       odometer fields
+       """,
+
+    'description': """
+        odometer fields
+        1.Purpose and kilometer run fields
+        2.Change the type of odoo meter value (float to integer)
+    """,
+
+    'author': "Loyal IT Solutions PVT LTD",
+    'website': "http://www.loyalitsolutions.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','fleet'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
