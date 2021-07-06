9018600b9b863739bf4a8584434f89c1ec8eeb4a
Hasna <73275743+hasna-has@users.noreply.github.com>
2021-07-06 09:57:42 +0530

    Add files via upload

--- payslip_expense_reference/models/models.py
+++ payslip_expense_reference/models/models.py
@@ -2,9 +2,38 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+from collections import namedtuple
+
+from datetime import datetime, date, timedelta, time
+from pytz import timezone, UTC
+
+from odoo import api, fields, models, SUPERUSER_ID, tools
+from odoo.addons.resource.models.resource import float_to_time, HOURS_PER_DAY
+from odoo.exceptions import AccessError, UserError, ValidationError
+
 
 # if worked_days.OTH.number_of_hours and contract.wage:
 #     result=(((contract.wage/31) / 8) * ( worked_days.OTH.number_of_hours)) * 1.50
+
+class HolidaysRequest(models.Model):
+    _inherit = "hr.leave"
+
+    hours_value = fields.Float(string="Hours Value",store=True)
+    hours_calc = fields.Boolean(string="Hours Calculation", store=True)
+
+    @api.onchange('number_of_hours_display')
+    def _onchange_value_number_of_hours_display(self):
+        for i in self:
+            i.hours_value = i.number_of_hours_display
+
+
+    @api.depends('number_of_days')
+    def _compute_number_of_hours_display(self):
+        res = super(HolidaysRequest, self)._compute_number_of_hours_display()
+        for i in self:
+            if i.hours_calc == True:
+                i.number_of_hours_display = i.hours_value
+        return res
 
 
 class HrPayslipInput(models.Model):
